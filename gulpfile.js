b8744dce18a55180c5ac168b3399ce1ed9556162
Brett Schwarz <brett.schwarz@gmail.com>
2018-04-02 08:32:36 -0700

    new dependecies

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,47 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
+var inlinecss = require('gulp-inline-css');
+var htmlmin = require('gulp-htmlmin');
 
+// For the index page and assets
 var DEST = 'dist/';
+gulp.task('main-js', function() {
+  return gulp.src(['js/*.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest(DEST + '/js/'));
+});
 
-gulp.task('default', function() {
-  return gulp.src('js/perfmatters.js')
-    // This will output the non-minified version
+gulp.task('main-css', function() {
+  return gulp.src(['css/*.css'])
+    .pipe(csso())
+    .pipe(gulp.dest(DEST + '/css/'));
+});
+
+gulp.task('main-html', function() {
+  return gulp.src(['*.html'])
+    .pipe(htmlmin())
+    .pipe(gulp.dest(DEST));
+});
+
+
+// for the pizza page and assets
+gulp.task('pizza-js', function() {
+  return gulp.src(['js/*.js', 'views/js/*.js'])
     .pipe(gulp.dest(DEST))
-    // This will minify and rename to foo.min.js
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest(DEST));
 });
+
+gulp.task('pizza-css', function() {
+  return gulp.src(['css/*.css', 'views/css/*.css'])
+    .pipe(gulp.dest(DEST))
+    .pipe(csso())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest(DEST));
+});
+
+
+gulp.task('main', ['main-html', 'main-js', 'main-css']);
