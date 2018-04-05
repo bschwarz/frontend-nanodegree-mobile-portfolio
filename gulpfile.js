693ceb99b11ddf3aa546ca57b16e8daf6e97cb2b
Brett Schwarz <brett.schwarz@gmail.com>
2018-04-04 21:09:38 -0700

    Adds html and images, and removes css cause within html task

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,40 @@
 var gulp = require('gulp');
-var rename = require('gulp-rename');
+// var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var csso = require('gulp-csso');
+// var csso = require('gulp-csso');
+// var cleancss = require('gulp-clean-css');
 var inlinecss = require('gulp-inline-css');
 var htmlmin = require('gulp-htmlmin');
 
-// For the index page and assets
+// Note: the '**/' is needed as a prefix to
+// preserve the directory structure.
+
+// To move and minify JS assests.
 var DEST = 'dist/';
-gulp.task('main-js', function() {
-  return gulp.src(['js/*.js'])
+gulp.task('js', function() {
+  return gulp.src(['**/js/*.js'])
     .pipe(uglify())
-    .pipe(gulp.dest(DEST + '/js/'));
+    .pipe(gulp.dest(DEST));
 });
 
-gulp.task('main-css', function() {
-  return gulp.src(['css/*.css'])
-    .pipe(csso())
-    .pipe(gulp.dest(DEST + '/css/'));
+// gulp.task('css', function() {
+//   return gulp.src(['**/css/*.css'])
+//     .pipe(cleancss())
+//     .pipe(gulp.dest(DEST));
+// });
+
+gulp.task('img', function() {
+  return gulp.src(['**/img/*.png', '**/img/*.jpg', '**/img/*.gif'])
+    .pipe(gulp.dest(DEST));
 });
 
-gulp.task('main-html', function() {
-  return gulp.src(['*.html'])
-    .pipe(htmlmin())
+gulp.task('html', function() {
+  // return gulp.src(['**/*.html', '!node_modules/', '!node_modules/**'])
+  return gulp.src(['index.html', 'project-*.html', '**/views/pizza.html'])
+    .pipe(inlinecss())
+    .pipe(htmlmin({collapseWhitespace: true,minifyJS:true,minifyCSS:true,removeComments:true}))
     .pipe(gulp.dest(DEST));
 });
 
 
-// for the pizza page and assets
-gulp.task('pizza-js', function() {
-  return gulp.src(['js/*.js', 'views/js/*.js'])
-    .pipe(gulp.dest(DEST))
-    .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest(DEST));
-});
-
-gulp.task('pizza-css', function() {
-  return gulp.src(['css/*.css', 'views/css/*.css'])
-    .pipe(gulp.dest(DEST))
-    .pipe(csso())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest(DEST));
-});
-
-
-gulp.task('main', ['main-html', 'main-js', 'main-css']);
+gulp.task('main', ['html', 'js', 'img']);
